d59a7bf7dc7aa08e327674bfc4509c3b632fe89e
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2023-09-12 09:46:14 -0300

    FIVE DAY 11/09/23
    OK

---
+++ viniport-servicos/inc/users.js
@@ -0,0 +1,74 @@
+var conn = require('./db')
+
+module.exports = {
+
+    render(res, req, error){
+
+        res.render('admin/login', {
+            body: req.body,
+            error
+        })
+
+
+    },
+
+    login(email, password){
+
+        return new Promise((s, f) =>{
+            conn.query(
+                `SELECT * FROM tb_users WHERE email = ?`,[
+                    email
+                ],(err, results) =>{
+
+                    if(err){
+                       f(err)
+                    }
+                    else{
+
+                        if(!results.length > 0){
+                          
+                            f('Usuário ou senha incorretos')
+                        }
+                        else{
+                            let row = results[0]
+
+                            if(row.password !== password){
+                                f('Usuário ou senha incorretos')
+                            }
+                            else{
+                                s(row)
+                            }
+                        }
+
+                    }
+
+                }
+)
+
+
+
+        })
+
+
+    },
+
+
+
+    getUsers(){
+
+        return new Promise((s, f) =>{
+
+            conn.query(`
+            SELECT * FROM tb_users`,(err, results) =>{
+    
+                if(err){
+                    f(err)
+                }
+                else{
+                    s(results)
+                }
+            });
+        });
+
+    }
+}
--- viniport-servicos/routes/admin.js
+++ viniport-servicos/routes/admin.js
@@ -1,9 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../inc/users')
 
 /* GET users listing. */
+
+router.use(function(req, res , next){
+
+  if(["/login"].indexOf(req.url) === -1 && !req.session.user) {
+      res.redirect('/admin/login')
+  } else{
+      next()
+  }
+
+
+})
+
 router.get('/', function(req, res, next) {
   res.render('admin/index', { title: 'Express'})
+});
+
+
+
+router.get('/login', function(req, res, next) {
+  users.render(res, req, null)
+});
+
+router.post('/login', function(req, res, next) {
+
+  if(!req.body.email){
+    users.render(res, req,  "Preencha o campo email!")
+}   
+  else if(!req.body.password){
+    users.render(res, req,  "Preencha o campo senha!")
+} 
+
+  else{
+      users.login(req.body.email, req.body.password)
+      .then(user =>{
+          console.log(user)
+          req.session.user = user
+          res.redirect('/admin')
+
+      }).catch(err =>{
+
+          console.log(err)
+          console.log('error')
+          users.render(res, req, err.message || err)
+
+      })
+
+
+} 
+
+
+})
+
+router.get('/logout', function(req, res, next){
+
+  delete req.session.user;
+
+  res.redirect('admin/login')
+
+})
+
+router.get('/employee', function(req, res, next) {
+  res.render('admin/employee', {title: 'ViniPort Servicos'})
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('admin/contacts', {title: 'ViniPort Servicos'})
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('admin/users', {title: 'ViniPort Servicos'})
+});
+
+router.get('/proposal', function(req, res, next) {
+  res.render('admin/proposal', {title: 'ViniPort Servicos'})
 });
 
 module.exports = router;
